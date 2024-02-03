41b7aa125d2a98f8f74d43fb36377a073cfd1c42
Lophane <138240731+Lophane@users.noreply.github.com>
2024-02-03 12:54:35 -0500

    Messed with the scriptable objects a bit

--- Assets/ScriptableObjects/Weapons/WeaponData.cs
+++ Assets/ScriptableObjects/Weapons/WeaponData.cs
@@ -5,7 +5,11 @@ public class WeaponData : ScriptableObject
 {
     public string weaponName;
     public GameObject weaponPrefab;
+    public AnimationClip attackAnimation;
+    public Vector2 hitboxSize;
+    public Vector2 hitboxOffset;
     public float damage;
     public float effectiveRange;
     public float attackSpeed;
+    public float armHealth;
 }
